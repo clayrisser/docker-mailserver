6f1568e287b85732da863927cb64b52fa1f32fbd
David Young <davidy@funkypenguin.co.nz>
2019-05-02 13:31:47 +1200

    Improved CI

---
+++ .circleci/prep-unit-tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Execute a few steps to ensure some of the more complex combinations of unit tests can pass
+mkdir -p helm-chart/docker-mailserver/config/opendkim/keys/example.com
+cp helm-chart/docker-mailserver/demo-mode-dkim-key-for-example.com.key helm-chart/docker-mailserver/config/opendkim/keys/example.com/mail.private
+echo "sample data for unit test" > helm-chart/docker-mailserver/config/opendkim/ignore.txt
