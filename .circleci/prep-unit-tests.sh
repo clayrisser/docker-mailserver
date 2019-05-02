1db3e029a791f99214153673e8b101ae8f9672b3
David Young <davidy@funkypenguin.co.nz>
2019-05-02 13:59:29 +1200

    Improved CI

--- .circleci/prep-unit-tests.sh
+++ .circleci/prep-unit-tests.sh
@@ -2,6 +2,7 @@
 # Execute a few steps to ensure some of the more complex combinations of unit tests can pass
 
 echo "Setting up environment for unit testing..."
+pwd
 
 mkdir -p project/helm-chart/docker-mailserver/config/opendkim/keys/example.com
 cp project/helm-chart/docker-mailserver/demo-mode-dkim-key-for-example.com.key project/helm-chart/docker-mailserver/config/opendkim/keys/example.com/mail.private
